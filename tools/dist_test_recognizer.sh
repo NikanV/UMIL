12ba38d63e1767e8dd988cf9241cdd8663906e8c
Nikan Vasei <94530582+NikanV@users.noreply.github.com>
2025-03-30 13:35:15 +0330

    Update dist_test_recognizer.sh

--- tools/dist_test_recognizer.sh
+++ tools/dist_test_recognizer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-PYTHON=${PYTHON:-"source $root_dir/miniconda3/bin/activate UMIL; python"}
+PYTHON=${PYTHON:-"python"}
 $PYTHON -m torch.distributed.launch --nproc_per_node=$1 main_umil.py -cfg configs/traffic/32_5.yaml --output output/test --only_test --pretrained output/test/best.pth
