2f2e762d26831457d510fe7f599f863e0d4254d4
NikanV <nikanvsiuni@gmail.com>
2025-04-05 20:09:07 +0330

    Updated configs for training

--- tools/dist_train_recognizer.sh
+++ tools/dist_train_recognizer.sh
@@ -2,4 +2,4 @@
 
 PYTHON=${PYTHON:-"python"}
 
-$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main.py -cfg configs/traffic/32_5.yaml --batch-size 2 --accumulation-steps 8 --output output/mil --pretrained k400_32_8.pth
+$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main.py -cfg configs/traffic/32_5.yaml --batch-size 2 --accumulation-steps 8 --output output/mil --pretrained /kaggle/input/umil-weights/k400_32_8.pth
