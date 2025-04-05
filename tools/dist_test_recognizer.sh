85f4b6233c2507505c2f1b21dd63f9e1dd0ce35e
NikanV <nikanvsiuni@gmail.com>
2025-04-05 20:10:22 +0330

    Updated configs for testing using k400 weight

--- tools/dist_test_recognizer.sh
+++ tools/dist_test_recognizer.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHON=${PYTHON:-"python"}
-$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main_umil.py -cfg configs/traffic/32_5.yaml --output output/test --only_test
+$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main_umil.py -cfg configs/traffic/32_5.yaml --output output/test --only_test --pretrained /kaggle/input/umil-weights/k400_32_8.pth
