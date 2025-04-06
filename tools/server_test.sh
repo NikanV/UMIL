26b6144c35464857acefc19e29047a3774daf917
NikanV <nikanvsiuni@gmail.com>
2025-04-06 12:42:56 +0330

    Bash command issue

--- tools/server_test.sh
+++ tools/server_test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 PYTHON=${PYTHON:-"python"}
-$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main_umil.py -cfg configs/traffic/32_5_server.yaml --output output/test --only_test --pretrained --pretrained /home/mnafez/UMIL/weights/k400_16_8.pth
+$PYTHON -m torch.distributed.launch --nproc_per_node=$1 main_umil.py -cfg configs/traffic/32_5_server.yaml --output output/test --only_test --pretrained /home/mnafez/UMIL/weights/k400_16_8.pth
