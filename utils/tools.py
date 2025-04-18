f7ea2ec3990d1cb457421ecc94b6670526c7f821
NikanV <nikanvsiuni@gmail.com>
2025-04-18 14:51:40 +0330

    debugging

--- utils/tools.py
+++ utils/tools.py
@@ -41,6 +41,7 @@ def evaluate_result(vid2abnormality, anno_file, root=''):
         videos[vid] = sub_video_gt
 
     for vid in videos:
+        print(f"Vid {vid}, Keys: {vid2abnormality.keys()}")
         if vid not in vid2abnormality.keys():
             print("The video %s is excluded on the result!" % vid)
             continue
